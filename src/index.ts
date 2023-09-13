73c7b9ceba083f7596c2cb1848c3c9c78038f1d3
Pat Barber <patbarber72@gmail.com>
2023-09-14 09:03:48 +1000

    mockserver init with file and string return

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,14 @@
+import cors from "@elysiajs/cors";
 import { Elysia } from "elysia";
-
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+const path = "src/ignored/mockResponses.json";
+const file = Bun.file(path);
+const aboutDetails = "";
+const app = new Elysia()
+  .use(cors())
+  .get("/", () => "MockServer Running")
+  .get("/alerts", () => file)
+  .get("/about", () => aboutDetails)
+  .listen(3000);
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
