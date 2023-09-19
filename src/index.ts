8fe662065ec6aa4f6666466f9b03a8e51828ffdd
Pat Barber <patbarber72@gmail.com>
2023-09-19 16:23:54 +1000

    more mock responses

--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,18 @@ import cors from "@elysiajs/cors";
 import { Elysia } from "elysia";
 const path = "src/ignored/mockResponses.json";
 const file = Bun.file(path);
-const aboutDetails = "";
+const docpath = "src/ignored/mockResponsesDocuments.json";
+const docfile = Bun.file(docpath);
+const membersPath = "src/ignored/mockMembersReponse.json";
+const membersfile = Bun.file(membersPath);
+const aboutDetails = ``;
 const app = new Elysia()
   .use(cors())
   .get("/", () => "MockServer Running")
   .get("/alerts", () => file)
   .get("/about", () => aboutDetails)
+  .get("/documents", () => docfile)
+  .get("/members", () => membersfile)
   .listen(3000);
 
 console.log(
